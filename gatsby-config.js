7177778a5a6c911514351e14ca9b7b669806e50d
MohamedTarek22 <201727036@std.sci.cu.edu.eg>
2021-06-25 15:24:21 +0200

    f

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
 module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
+    description:`Sign language`,
+    author: `my`
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
-};
+  plugins:  [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/loveyou_emoji.svg`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
+
